1211ba39609c29570546ad384b9cf2bdddf9d652
adexholdingstrust <tenantservices@adexholdings.com>
2025-12-26 14:52:04 -0800

    Update data.js

--- assets/data.js
+++ assets/data.js
@@ -117,7 +117,8 @@ window.ADEX_DATA = {
           "https://maps.app.goo.gl/h8Kza81BevSNPA1d7",
       streetViewEmbed:
           "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4269.247031157026!2d3.567563310848142!3d6.436470924141108!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103bf7e5ce493465%3A0x436e208846a77b99!2sSterling%20Cooperative%20Estate!5e1!3m2!1sen!2sus!4v1766785908479!5m2!1sen!2sus&output=svembed",
-      photos: [],
+      photos: ["/assets/photos/sterling/sterling1.jpg",
+                "/assets/photos/sterling/sterling2.jpg",],
       video: {
         type: "youtube", // youtube | vimeo | mp4
         url: "https://www.youtube.com/watch?v=XXXX"
